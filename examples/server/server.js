ec67de238175dccea18ba9a11b4bea1a77d6ca69
Dalton Rowe <cdaltonrowe@gmail.com>
2019-09-07 12:26:12 -0500

    update for express

--- examples/server/server.js
+++ examples/server/server.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Serve our example pages as the root
+app.use("/", express.static(path.join(__dirname, "../pages")));
 
+// Serve webpack javascript bundles
+app.use("/bundles", express.static(path.join(__dirname, "../dist")));
+
+// Serve turbolinks source code
+app.use(
+  "/turbolinks-src",
+  express.static(path.join(__dirname, "../../src"), {
+    setHeaders: function(res, path, stat) {
+      res.set("Content-Type", "text/plain"); // Use express.static() options to set headers
+    }
+  })
+);
+
+// Hey, Listen!
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
