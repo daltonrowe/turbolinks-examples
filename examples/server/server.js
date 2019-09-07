1f0c86bf4561ec3807cbffe929487099d1ad904f
Dalton Rowe <cdaltonrowe@gmail.com>
2019-09-07 12:32:41 -0500

    add comment

--- examples/server/server.js
+++ examples/server/server.js
@@ -19,7 +19,7 @@ app.use(
   "/turbolinks-src",
   express.static(path.join(__dirname, "../../src"), {
     setHeaders: function(res, path, stat) {
-      res.set("Content-Type", "text/plain"); // Use express.static() options to set headers
+      res.set("Content-Type", "text/plain"); // Set headers to text plain for easier frontend
     }
   })
 );
