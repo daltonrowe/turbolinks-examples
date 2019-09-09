6526003b779f0a6fdceefaf77f22982c6c99070d
Dalton Rowe <cdaltonrowe@gmail.com>
2019-09-09 12:55:17 -0500

    add reload example

---
+++ examples/src/TurbolinksExamplesTrackReload.ts
@@ -0,0 +1,8 @@
+class TurbolinksExamplesTrackReload {
+   shout() {
+      console.log('TrackReload Script!');
+   }
+}
+
+const trackReloadExample = new TurbolinksExamplesTrackReload;
+trackReloadExample.shout();
