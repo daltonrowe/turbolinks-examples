cf99d662fe50d4dcf2d3d3965456039d1a28186c
Dalton Rowe <cdaltonrowe@gmail.com>
2019-09-10 22:26:10 -0500

    identical scripts behavior

--- examples/src/TurbolinksExamplesTrackReload.ts
+++ examples/src/TurbolinksExamplesTrackReload.ts
@@ -1,6 +1,17 @@
 class TurbolinksExamplesTrackReload {
+   numberOfLoadEvents: number;
+
+   constructor() {
+      this.numberOfLoadEvents = 0;
+   }
+
    shout() {
       console.log('TrackReload Script!');
+
+      addEventListener('turbolinks:load', () => {
+         this.numberOfLoadEvents++
+         console.log('Load Number: ', this.numberOfLoadEvents);
+      })
    }
 }
 
